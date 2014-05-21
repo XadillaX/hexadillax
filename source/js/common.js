b47c94f175003c53171cc530868b1dad0df0a983
XadillaX <admin@xcoder.in>
2014-05-21 17:33:33 +0800

    初步完成版

--- source/js/common.js
+++ source/js/common.js
@@ -4,6 +4,29 @@ function random(max) {
 
 $(function() {
     var idx = random(backArray.length - 1);
-    $.backstretch("/images/background/" + backArray[idx]);
-    console.log("/images/background/" + backArray[idx]);
+    //var idx = backArray.length - 1;
+    $.backstretch(backRoot + backArray[idx]);
+    //console.log("/images/background/" + backArray[idx]);
+    
+    $("nav[role=banner]").headroom({
+        offset  : 10,
+        onNotTop: function() {
+            $("nav[role=banner]").stop().animate({ top: "-50px" });
+        },
+        onTop   : function() {
+            $("nav[role=banner]").stop().animate({ top: "0" });
+        },
+        onPin   : function() {
+            $("nav[role=banner]").stop().animate({ top: "0" });
+        },
+        onUnpin : function() {
+            $("nav[role=banner]").stop().animate({ top: "-50px" });
+        }
+    });
+    
+    $(".post-body p").each(function() {
+        if($(this).find("img").length) {
+            $(this).css("text-align", "center");
+        }
+    });
 });
